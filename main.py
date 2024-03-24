9aa00f424b5b5c087a4be978a5030d120b2427d4
Haseeb Shah <haseebs@outlook.com>
2024-03-24 00:02:03 -0700

    add support for defining and logging nested configs

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import numpy as np
 import logging
 
 from datetime import timedelta
+from rich.pretty import pretty_repr
 from timeit import default_timer as timer
 from omegaconf import DictConfig, OmegaConf
 from hydra.core.hydra_config import HydraConfig
 
 from utils import utils
+from utils.utils import prep_cfg_for_db
 from experiment import ExperimentManager, Metric
 
 log = logging.getLogger(__name__)
@@ -17,36 +19,30 @@ log = logging.getLogger(__name__)
 @hydra.main(version_base="1.3", config_path="configs", config_name="config")
 def main(cfg: DictConfig) -> None:
     start = timer()
-    args = cfg.args
     if HydraConfig.get().mode.value == 2:  # check whether its a sweep
-        args.run += HydraConfig.get().job.num
-        log.info(f'Running sweep... Run ID: {args.run}')
-    log.info(OmegaConf.to_yaml({**args, **args})) # change 2nd args to another nested config
-    exp = ExperimentManager(args.db_name, {**args,**cfg.agent}, args.db_prefix, cfg.db)
+        cfg.run += HydraConfig.get().job.num
+        log.info(f'Running sweep... Run ID: {cfg.run}')
+    log.info(f"Output directory  : {hydra.core.hydra_config.HydraConfig.get().runtime.output_dir}")
+    flattened_cfg = prep_cfg_for_db(OmegaConf.to_container(cfg), to_remove=["schema", "db"])
+    log.info(pretty_repr(flattened_cfg))
+    exp = ExperimentManager(cfg.db_name, flattened_cfg, cfg.db_prefix, cfg.db)
     tables = {}
     for table_name in list(cfg.schema.keys()):
         columns = cfg.schema[table_name].columns
         primary_keys = cfg.schema[table_name].primary_keys
         tables[table_name] = Metric(table_name, columns, primary_keys, exp)
 
-    utils.set_seed(args.seed)
+    utils.set_seed(cfg.seed)
 
     for step in range(0, 100):
-        tables["errors"].add_data([args.run,
-                                   step,
-                                   random.random(),
-                                   "teest",
-                                   [1,2,3,4],
-                                   {"param1": 1,
-                                    "param2": 3}
-                                   ])
+        tables["errors"].add_data([cfg.run, step, random.random()])
         if not step % 10:
             tables["errors"].commit_to_database()
 
     total_time = timedelta(seconds=timer() - start).seconds / 60
     tables["summary"].add_data(
         [
-            args.run,
+            cfg.run,
             step,
             random.random(),
             total_time
@@ -57,6 +53,6 @@ def main(cfg: DictConfig) -> None:
     log.info(f'Total time taken: {total_time}  minutes')
 
 
-
 if __name__ == "__main__":
     main()
+
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@ import random
 
 import torch
 import numpy as np
+from collections.abc import MutableMapping
 
 
 def set_seed(seed):
@@ -9,5 +10,23 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     random.seed(seed)
     np.random.seed(seed)
-    # torch.use_deterministic_algorithms(True) # cba
+    # torch.use_deterministic_algorithms(True) # causes errors, cba...
     torch.backends.cudnn.deterministic = True
+
+
+def flatten_dict(dictionary: dict, parent_key: str='', separator: str='_') -> dict:
+    items = []
+    for key, value in dictionary.items():
+        new_key = parent_key + separator + key if parent_key else key
+        if isinstance(value, MutableMapping):
+            items.extend(flatten_dict(value, new_key, separator=separator).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+
+def prep_cfg_for_db(cfg: dict, to_remove: list[str]) -> dict:
+    for key in to_remove:
+        del cfg[key]
+    return flatten_dict(cfg)
+
